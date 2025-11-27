527210659428feef47415a0c7585b81db4253a9c
wallcrack <wallcrack@163.com>
2025-11-27 19:12:45 +0800

    实现了添加，查看，删除任务的功能😘

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,112 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result, anyhow};
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use serde_json::{self};
+use std::fs;
+use std::path::Path;
+use time::OffsetDateTime;
+
+fn create_file_with_dirs(path: impl AsRef<Path>) -> std::io::Result<fs::File> {
+    let path = path.as_ref();
+    if let Some(parent) = path.parent() {
+        if !parent.exists() {
+            fs::create_dir_all(parent)?; // 自动递归创建所有目录
+        }
+    }
+    fs::File::create(path)
+}
+
+fn first_run() {
+    let file_path = Path::new("archive/Task.json");
+    if !file_path.exists() {
+        if let Ok(_) = create_file_with_dirs(file_path) {
+            println!("File created successfully!");
+        } else {
+            println!("Failed to create file!");
+        }
+    }
+}
+
+#[derive(Debug, Parser)]
+struct CLI {
+    option: String,
+    content: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Task {
+    content: String,
+    create_date: OffsetDateTime,
+}
+
+fn load_tasks() -> Result<Vec<Task>> {
+    let file_path = Path::new("archive/Task.json");
+    if !file_path.exists() {
+        return Ok(vec![]);
+    }
+    let content = fs::read(&file_path).context("Failed to read file!")?;
+    if content.is_empty() {
+        return Ok(vec![]);
+    }
+    let tasks: Vec<Task> =
+        serde_json::from_slice(&content).context("Failed to deserialize tasks!")?;
+    Ok(tasks)
+}
+
+fn save_tasks(tasks: &Vec<Task>) -> Result<()> {
+    let json = serde_json::to_string_pretty(tasks)?;
+    fs::write("archive/Task.json", json).context("Failed to write file!")?;
+    Ok(())
+}
+
+fn add_task(content: String) -> Result<()> {
+    let current_task = Task {
+        content,
+        create_date: OffsetDateTime::now_local().unwrap(),
+    };
+    let mut tasks = load_tasks().context("Failed to load tasks")?;
+    tasks.push(current_task);
+    save_tasks(&tasks).context("Failed to save tasks!")?;
+    Ok(())
+}
+
+fn check_tasks() -> Result<()> {
+    let tasks = load_tasks().context("Failed to load tasks")?;
+    let mut index = 0;
+    for task in tasks {
+        index += 1;
+        println!("{}: {}\n  {}", index, task.content, task.create_date);
+    }
+    Ok(())
+}
+
+fn delete_task(content: String) -> Result<()> {
+    let mut tasks = load_tasks().context("Failed to load tasks")?;
+
+    let index = content.trim().parse::<i32>().context("Invalid number!")?;
+    if index <= 0 || index > tasks.len() as i32 {
+        return Err(anyhow!("Invalid index!"));
+    }
+    tasks.remove((index - 1) as usize);
+    save_tasks(&tasks).context("Failed to save tasks!")?;
+    Ok(())
+}
+
+fn clear_tasks() -> Result<()> {
+    let empty_list = Vec::<Task>::new();
+    save_tasks(&empty_list).context("Failed to save tasks!")?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = CLI::parse();
+    first_run();
+    match args.option.as_str() {
+        "add" => add_task(args.content)?,
+        "check" => check_tasks()?,
+        "delete" => delete_task(args.content)?,
+        "clear" => clear_tasks()?,
+        _ => println!("Invalid option"),
+    }
+    Ok(())
 }
